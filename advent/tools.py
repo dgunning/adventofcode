a8e81f85b491111a75fd242e144db7056fb76fc7
Dwight Gunning <dgunning@gmail.com>
2021-12-05 10:40:33 -0500

    Refactored day 5

--- advent/tools.py
+++ advent/tools.py
@@ -3,6 +3,7 @@ from .pytudes import *
 from typing import Tuple
 from dataclasses import dataclass
 from collections import Counter
+from functools import cached_property
 
 Instruction = Tuple[str, int]
 
