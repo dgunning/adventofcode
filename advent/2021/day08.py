fb78aa0eaced6a215b02dd27b22c9f5d3fa02459
Dwight Gunning <dgunning@gmail.com>
2021-12-08 17:02:08 -0500

    Add day 8 solution

--- advent/2021/day08.py
+++ advent/2021/day08.py
@@ -9,44 +9,47 @@ def day8_1(puzzle_input: List[Tuple]):
                 ])
 
 
-def get_digits(output):
-    return None
-
-
 def day8_2(puzzle_input: List[Tuple]):
-    signals = {'abcdefg': 8, 'bcdef': 5, 'acdfg': 2, 'abcdf': 3, 'abd': 7, 'abcdef': 9, 'bcdefg': 6, 'abef': 4,
-               'abcdeg': 0, 'ab': 1}
-    result = [[signals.get(''.join(sorted(output)))
-               for output in outputs]
-              for signals, outputs in puzzle_input
-              ]
-    return None
+    digit_length_map = {2: '1', 3: '7', 4: '4', 7: '8'}
 
+    decoded_outputs = []
+    for segments, outputs in puzzle_input:
+        codes = {digit_length_map.get(len(o)): set(o)
+                 for o in list(segments) + list(outputs)
+                 if len(o) in digit_length_map
+                 }
 
-def parse_input(line):
-    signal, _, output = line.partition(' | ')
-    return atoms(signal), atoms(output)
+        digits = ''
+        for output in outputs:
+            outs = set(output)
+            if len(output) == 5:
+                if outs.issuperset(codes['7']):
+                    digits += '3'
+                elif outs.issuperset(codes['4'] - codes['1']):
+                    digits += '5'
+                else:
+                    digits += '2'
+            elif len(output) == 6:
+                if outs.issuperset(codes['4']):
+                    digits += '9'
+                elif len(outs & codes['1']) == 1:
+                    digits += '6'
+                else:
+                    digits += '0'
+            else:
+                digits += digit_length_map[len(outs)]
+
+        decoded_outputs.append(int(digits))
+    return sum(decoded_outputs)
 
 
 if __name__ == '__main__':
+    def parse_input(line):
+        signal, _, output = line.partition(' | ')
+        return atoms(signal), atoms(output)
+
+
     in8 = data(8, parse_input)
 
-    test_input = """
-be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
-    """
-    testlines = list(tuple(map(parse_input, [line for line in test_input.split('\n') if line])))
-    print(day8_2(testlines))
-
-    """
     with binding(**globals()):
         print(do(8))
-    """
